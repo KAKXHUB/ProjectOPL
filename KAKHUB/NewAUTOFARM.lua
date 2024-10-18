b8ae3f223450d5e1799aa4588ddc9d5007a16318
KAKXHUB <tanapatzz.1234@gmail.com>
2024-10-19 02:42:45 +0700

    2.2.0

--- KAKHUB/NewAUTOFARM.lua
+++ KAKHUB/NewAUTOFARM.lua
@@ -1,4 +1,4 @@
-local Versionxx = "2.1.9"
+local Versionxx = "2.2.0"
 print("Version: "..Versionxx)
 ---------------
 
@@ -1886,9 +1886,8 @@ do
     spawn(function()
         while wait() do
             pcall(function()
-                if not Options.MyToggleAutoKillMonter.Value and game:GetService("Players").LocalPlayer.PlayerGui.Challenges.Frame.Frame.DailyFrame.ScrollingFrame.Challenge_3.Claim.AutoButtonColor == false then return end;
+                if not Options.MyToggleAutoKillMonter.Value and game:GetService("Players").LocalPlayer.PlayerGui.Challenges.Frame.Frame.DailyFrame.ScrollingFrame.Challenge_3.Claim.TextLabel.Text == "Claim" then return end;
                 print("Work")
-
             end)
         end
     end);
